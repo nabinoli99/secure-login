0bc93bea04c1e1e3c35f0e658ca381ef436ba71f
Suckscorpio999 <olinabin214@gmail.com>
2026-02-21 13:33:20 +0545

    feat(auth): add UserRepository with username lookup support

---
+++ src/main/java/in/nabin/springsecurity/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package in.nabin.springsecurity.repository;
+
+import in.nabin.springsecurity.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser,Long>
+{
+    Optional<AppUser> findByUsername(String username);
+}
