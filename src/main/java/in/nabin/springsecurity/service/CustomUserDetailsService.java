00107b56e63634714d32d101780c82bf80bb6a58
Suckscorpio999 <olinabin214@gmail.com>
2026-02-21 13:33:09 +0545

    feat(auth): implement CustomUserDetailsService for database authentication

---
+++ src/main/java/in/nabin/springsecurity/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package in.nabin.springsecurity.service;
+
+import in.nabin.springsecurity.entities.AppUser;
+import in.nabin.springsecurity.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService
+{
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        AppUser user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return User.withUsername(user.getUsername())
+                .password(user.getPassword()) // encoded password from DB
+                .roles(user.getRole())
+                .build();
+    }
+}
