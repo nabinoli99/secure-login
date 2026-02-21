d6331c96ece6523f3dd55a21b1b1745c2ccc3e05
Suckscorpio999 <olinabin214@gmail.com>
2026-02-21 13:31:47 +0545

    feat: add AppUser entity for authentication and role-based access

---
+++ src/main/java/in/nabin/springsecurity/entities/AppUser.java
@@ -0,0 +1,54 @@
+package in.nabin.springsecurity.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class AppUser
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
