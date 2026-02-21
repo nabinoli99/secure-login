ce8e0da804598a583c4c4971f6254c5dbdf4a3f8
Suckscorpio999 <olinabin214@gmail.com>
2026-02-21 13:32:05 +0545

    feat: add AppX main application class and entry point

---
+++ src/main/java/in/nabin/springsecurity/AppX.java
@@ -0,0 +1,13 @@
+package in.nabin.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppX {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppX.class, args);
+    }
+
+}
