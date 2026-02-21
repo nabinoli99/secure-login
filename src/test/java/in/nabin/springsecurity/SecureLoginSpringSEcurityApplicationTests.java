dfa6e6b1a14a7c36d9a029990895dab33d9dac4e
Suckscorpio999 <olinabin214@gmail.com>
2026-02-21 13:33:28 +0545

    test: add context loading test for security application

---
+++ src/test/java/in/nabin/springsecurity/SecureLoginSpringSEcurityApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nabin.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureLoginSpringSEcurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
